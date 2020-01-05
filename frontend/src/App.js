4ba6774608c1c9dee1aa04445206f3e168c540e7
GPrince00 <gustavoprince0@gmail.com>
2020-01-05 17:59:14 -0300

    feat(Login): Created a login page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import './App.css';
 
+import Login from './pages/login';
+
 function App() {
   return (
-    <h1>Hello Prince</h1>
+    <Login></Login>
   );
 }
 
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-// import logo from 
 import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
