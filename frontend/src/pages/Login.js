f32c92ead6c868adbfa93a8ce2fea3a8f7008db8
GPrince00 <gustavoprince0@gmail.com>
2020-01-07 15:31:57 -0300

    feat(api): Created a service api that communicate with tindev-api

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,13 +1,21 @@
 import React, { useState} from 'react';
 import './Login.css';
 
+import api from '../services/api';
+
 // import logo from '../assets/logo.svg';
 
 export default function Login({ history }) {
+    const [username, setUsername]= useState('');
     
     async function handleSubmit(e) {
         e.preventDefault();
      
+        const response = await api.post('/devs', {
+            username: username,
+        });
+        
+        const { id } = response.data;
 
         history.push('/main')
     }
@@ -17,7 +25,11 @@ export default function Login({ history }) {
         <div className="login-container">
             <form onSubmit={handleSubmit}>
                 <h1>LOGO</h1>
-                <input placeholder="Digite seu usuário no Github"/>
+                <input 
+                    placeholder="Digite seu usuário no Github"
+                    value={username}
+                    onChange={e => setUsername(e.target.value)}
+                />
                 <button type="submit">Enviar</button>
             </form>
         </div>
